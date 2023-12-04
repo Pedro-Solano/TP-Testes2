e2e06636e91778bb993dd6f891dbc4a51cc93260
Pedro-Solano <p.solano1000@gmail.com>
2023-12-04 19:44:09 -0300

    Update billDomain.test.js

--- backend/__tests__/billDomain.test.js
+++ backend/__tests__/billDomain.test.js
@@ -11,52 +11,52 @@ jest.mock('../adapters/billRepository.js', () => {
   };
 });
 
-describe('billDomain registerBill', () => {
-    let userRepository;
-    let newBill;
+// describe('billDomain registerBill', () => {
+//     let userRepository;
+//     let newBill;
   
-    beforeEach(() => {
-      userRepository = {
-        add: jest.fn(),
-      };
-      billDomain.configRepo(userRepository);
+//     beforeEach(() => {
+//       userRepository = {
+//         add: jest.fn(),
+//       };
+//       billDomain.configRepo(userRepository);
   
-      newBill = new Bill(1, 1, 'Conta de Luz', 100, 'fixa', false, true, 3, new Date('2023-01-01'), new Date('2023-01-05'));
-    });
+//       newBill = new Bill(1, 1, 'Conta de Luz', 100, 'fixa', false, true, 3, new Date('2023-01-01'), new Date('2023-01-05'));
+//     });
   
-    // test('Deve cadastrar uma conta única com sucesso', async () => {
-    //   // Configuração para cadastrar uma única conta
-    //   newBill.divide = false;
-    //   newBill.repeat = false;
+//     test('Deve cadastrar uma conta única com sucesso', async () => {
+//       // Configuração para cadastrar uma única conta
+//       newBill.divide = false;
+//       newBill.repeat = false;
   
-    //   const result = await billDomain.registerBill(1, 'Conta única', 200, 'fixa', false, false, 1, new Date('2023-01-01'), new Date('2023-01-05'));
+//       const result = await billDomain.registerBill(1, 'Conta única', 200, 'fixa', false, false, 1, new Date('2023-01-01'), new Date('2023-01-05'));
   
-    //   expect(result).toEqual({ message: 'Conta cadastrada com sucesso!' });
-    //   expect(userRepository.add).toHaveBeenCalledWith(newBill);
-    // });
+//       expect(result).toEqual({ message: 'Conta cadastrada com sucesso!' });
+//       expect(userRepository.add).toHaveBeenCalledWith(newBill);
+//     });
   
-    // test('Deve cadastrar uma conta dividida com sucesso', async () => {
-    //   // Configuração para cadastrar uma conta dividida
-    //   newBill.divide = true;
-    //   newBill.repeat = false;
+//     test('Deve cadastrar uma conta dividida com sucesso', async () => {
+//       // Configuração para cadastrar uma conta dividida
+//       newBill.divide = true;
+//       newBill.repeat = false;
   
-    //   const result = await billDomain.registerBill(1, 'Conta dividida', 300, 'fixa', true, false, 3, new Date('2023-01-01'), new Date('2023-01-05'));
+//       const result = await billDomain.registerBill(1, 'Conta dividida', 300, 'fixa', true, false, 3, new Date('2023-01-01'), new Date('2023-01-05'));
   
-    //   expect(result).toEqual({ message: 'Conta cadastrada com sucesso!' });
-    //   expect(userRepository.add).toHaveBeenCalledTimes(3);
-    // });
+//       expect(result).toEqual({ message: 'Conta cadastrada com sucesso!' });
+//       expect(userRepository.add).toHaveBeenCalledTimes(3);
+//     });
   
-    // test('Deve cadastrar uma conta repetida com sucesso', async () => {
-    //   // Configuração para cadastrar uma conta repetida
-    //   newBill.divide = false;
-    //   newBill.repeat = true;
+//     test('Deve cadastrar uma conta repetida com sucesso', async () => {
+//       // Configuração para cadastrar uma conta repetida
+//       newBill.divide = false;
+//       newBill.repeat = true;
   
-    //   const result = await billDomain.registerBill(1, 'Conta repetida', 400, 'fixa', false, true, 2, new Date('2023-01-01'), new Date('2023-01-05'));
+//       const result = await billDomain.registerBill(1, 'Conta repetida', 400, 'fixa', false, true, 2, new Date('2023-01-01'), new Date('2023-01-05'));
   
-    //   expect(result).toEqual({ message: 'Conta cadastrada com sucesso!' });
-    //   expect(userRepository.add).toHaveBeenCalledTimes(2);
-    // });
-  });
+//       expect(result).toEqual({ message: 'Conta cadastrada com sucesso!' });
+//       expect(userRepository.add).toHaveBeenCalledTimes(2);
+//     });
+//   });
 
   describe('billDomain payBill', () => {
     let userRepository;
