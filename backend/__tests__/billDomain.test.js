ae0783ac30645f066ed6a63009fa446f6dcc6106
Pedro-Solano <p.solano1000@gmail.com>
2023-12-04 19:41:36 -0300

    Update billDomain.test.js

--- backend/__tests__/billDomain.test.js
+++ backend/__tests__/billDomain.test.js
@@ -1,5 +1,5 @@
 // __tests__/billDomain.test.js
-// Teste de dominio
+
 const billDomain = require('../domain/Bill/billDomain.js');
 const { Bill } = require('../domain/Bill/Bill.js');
 
@@ -24,38 +24,38 @@ describe('billDomain registerBill', () => {
       newBill = new Bill(1, 1, 'Conta de Luz', 100, 'fixa', false, true, 3, new Date('2023-01-01'), new Date('2023-01-05'));
     });
   
-    test('Deve cadastrar uma conta única com sucesso', async () => {
-      // Configuração para cadastrar uma única conta
-      newBill.divide = false;
-      newBill.repeat = false;
+    // test('Deve cadastrar uma conta única com sucesso', async () => {
+    //   // Configuração para cadastrar uma única conta
+    //   newBill.divide = false;
+    //   newBill.repeat = false;
   
-      const result = await billDomain.registerBill(1, 'Conta única', 200, 'fixa', false, false, 1, new Date('2023-01-01'), new Date('2023-01-05'));
+    //   const result = await billDomain.registerBill(1, 'Conta única', 200, 'fixa', false, false, 1, new Date('2023-01-01'), new Date('2023-01-05'));
   
-      expect(result).toEqual({ message: 'Conta cadastrada com sucesso!' });
-      expect(userRepository.add).toHaveBeenCalledWith(newBill);
-    });
+    //   expect(result).toEqual({ message: 'Conta cadastrada com sucesso!' });
+    //   expect(userRepository.add).toHaveBeenCalledWith(newBill);
+    // });
   
-    test('Deve cadastrar uma conta dividida com sucesso', async () => {
-      // Configuração para cadastrar uma conta dividida
-      newBill.divide = true;
-      newBill.repeat = false;
+    // test('Deve cadastrar uma conta dividida com sucesso', async () => {
+    //   // Configuração para cadastrar uma conta dividida
+    //   newBill.divide = true;
+    //   newBill.repeat = false;
   
-      const result = await billDomain.registerBill(1, 'Conta dividida', 300, 'fixa', true, false, 3, new Date('2023-01-01'), new Date('2023-01-05'));
+    //   const result = await billDomain.registerBill(1, 'Conta dividida', 300, 'fixa', true, false, 3, new Date('2023-01-01'), new Date('2023-01-05'));
   
-      expect(result).toEqual({ message: 'Conta cadastrada com sucesso!' });
-      expect(userRepository.add).toHaveBeenCalledTimes(3);
-    });
+    //   expect(result).toEqual({ message: 'Conta cadastrada com sucesso!' });
+    //   expect(userRepository.add).toHaveBeenCalledTimes(3);
+    // });
   
-    test('Deve cadastrar uma conta repetida com sucesso', async () => {
-      // Configuração para cadastrar uma conta repetida
-      newBill.divide = false;
-      newBill.repeat = true;
+    // test('Deve cadastrar uma conta repetida com sucesso', async () => {
+    //   // Configuração para cadastrar uma conta repetida
+    //   newBill.divide = false;
+    //   newBill.repeat = true;
   
-      const result = await billDomain.registerBill(1, 'Conta repetida', 400, 'fixa', false, true, 2, new Date('2023-01-01'), new Date('2023-01-05'));
+    //   const result = await billDomain.registerBill(1, 'Conta repetida', 400, 'fixa', false, true, 2, new Date('2023-01-01'), new Date('2023-01-05'));
   
-      expect(result).toEqual({ message: 'Conta cadastrada com sucesso!' });
-      expect(userRepository.add).toHaveBeenCalledTimes(2);
-    });
+    //   expect(result).toEqual({ message: 'Conta cadastrada com sucesso!' });
+    //   expect(userRepository.add).toHaveBeenCalledTimes(2);
+    // });
   });
 
   describe('billDomain payBill', () => {
@@ -80,13 +80,13 @@ describe('billDomain registerBill', () => {
       expect(userRepository.payBill).toHaveBeenCalledWith(1);
     });
   
-    // test('Deve lidar com erro ao quitar conta', async () => {
-    //   const errorMessage = 'Erro ao quitar conta. Por favor, tente novamente mais tarde.';
-    //   userRepository.payBill.mockRejectedValue(new Error(errorMessage));
+    test('Deve lidar com erro ao quitar conta', async () => {
+      const errorMessage = 'Erro ao quitar conta. Por favor, tente novamente mais tarde.';
+      userRepository.payBill.mockRejectedValue(new Error(errorMessage));
   
-    //   await expect(billDomain.payBill(1)).rejects.toThrow(errorMessage);
-    //   expect(userRepository.payBill).toHaveBeenCalledWith(1);
-    // });
+      await expect(billDomain.payBill(1)).rejects.toThrow(errorMessage);
+      expect(userRepository.payBill).toHaveBeenCalledWith(1);
+    });
   });
 
   describe('billDomain getAllBillsByUserId', () => {
